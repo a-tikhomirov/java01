c5db3319d7b911de8fef3e631712104eecdf61f2
Andrey Tikhomirov <andrey.tikhomirov.88@gmail.com>
2019-10-31 10:52:40 +0300

    added java project

---
+++ lesson01/src/ru/geekbrains/javaone/lesson01/hw/Lesson01HW.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.javaone.lesson01.hw;
+/**
+ * A program that contains Java 1 lesson 1 Home Work
+ *
+ * @author Andrey Tikhomirov
+ * @version 1.0
+ */
+public class Lesson01HW {
+    public static void main(String[] args) {
+        
+    }
+}
