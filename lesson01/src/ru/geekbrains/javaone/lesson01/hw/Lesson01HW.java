c061338b58d548d21bca1fc8a45c7ade086935d4
a-tikhomirov <andrey.tikhomirov.88@gmail.com>
2019-10-31 12:32:16 +0300

    lesson 1 task 8

--- lesson01/src/ru/geekbrains/javaone/lesson01/hw/Lesson01HW.java
+++ lesson01/src/ru/geekbrains/javaone/lesson01/hw/Lesson01HW.java
@@ -66,4 +66,16 @@ public class Lesson01HW {
     private static void sayHello(String name){
         System.out.println("Привет, " + name + "!");
     }
+
+    //8. * Написать метод, который определяет является ли год високосным, и выводит сообщение в консоль. Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
+    private static void checkLeapYear(int year){
+        if (year <= 0){
+            System.out.println("Передаваемый параметр должен быть положительным числом");
+            return;
+        }
+
+        boolean isLeap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+
+        System.out.println("Год " + year + (isLeap ? " " : " не ") + "является високосным");
+    } 
 }
