0333b9f75479de5c495e9e543a2a780830487c57
a-tikhomirov <andrey.tikhomirov.88@gmail.com>
2019-11-01 14:33:40 +0300

    lesson 2 hw init

---
+++ lesson02/src/ru/geekbrains/javaone/lesson02/hw/Lesson02HW.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.javaone.lesson02.hw;
+/**
+ * A program that contains Java 1 lesson 2 Home Work
+ *
+ * @author Andrey Tikhomirov
+ * @version 1.0
+ */
+public class Lesson02HW {
+    public static void main(String[] args) {
+        // ...
+    }
+}
