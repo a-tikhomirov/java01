c00aed1888922db41d03ffe01c0eff40f82b185b
a-tikhomirov <andrey.tikhomirov.88@gmail.com>
2019-11-10 14:56:13 +0300

    lesson 4 task 7

--- lesson04/src/ru/geekbrains/lesson04/hw/Cooperator.java
+++ lesson04/src/ru/geekbrains/lesson04/hw/Cooperator.java
@@ -1,13 +1,23 @@
 package ru.geekbrains.lesson04.hw;
 
 public class Cooperator {
+    private static int idCounter = 1;
+
+    private int id;
     private String fio;
     private int age;
     private String phoneNumber;
     private String post;
     private int salary;
 
+    // 7. Продумать конструктор таким образом, чтобы при создании каждому сотруднику присваивался личный уникальный идентификационный порядковый номер
+    private Cooperator() {
+        id = idCounter;
+        ++idCounter;
+    }
+
     Cooperator(String fio, int age, String phoneNumber, String post, int salary) {
+        this();
         this.fio = fio;
         this.age = age;
         this.phoneNumber = phoneNumber;
@@ -15,6 +25,9 @@ public class Cooperator {
         this.salary = salary;
     }
 
+    public int gedId() {
+        return id;
+    }
     public String getFio() {
         return fio;
     }
@@ -36,11 +49,20 @@ public class Cooperator {
 
     public String getCoopInfo() {
         StringBuilder sb = new StringBuilder();
+        sb.append("id:\t\t\t" + id + "\n");
         sb.append("ФИО:\t\t" + fio + "\n");
         sb.append("Возраст:\t" + age + "\n");
         sb.append("Номер тел.:\t" + phoneNumber + "\n");
         sb.append("Должность:\t" + post + "\n");
         sb.append("Зарплата:\t" + salary + "\n");
         return sb.toString();
+    }
+
+    public boolean isAgeBetween(int age1, int age2) {
+        return age > age1 && age < age2;
+    }
+
+    public void moreSalary(int delta) {
+        this.salary += delta;
     }
 }
--- lesson04/src/ru/geekbrains/lesson04/hw/Leeson04HW.java
+++ lesson04/src/ru/geekbrains/lesson04/hw/Leeson04HW.java
@@ -1,15 +1,15 @@
-package ru.geekbrains.lesson04.hw; // ...
-
+package ru.geekbrains.lesson04.hw;
+// ...
 public class Leeson04HW {
     public static void main(String[] args) {
-        // 4. Вывести при помощи методов из пункта 3 ФИО и должность
-        Cooperator co1 = new Cooperator("Сквозник-Дмухановский Антон Антонович", 60, "+7-999-999-10-10", "директор", 180000);
+        // 4. Вывести при помощи методов класса ФИО и должность
+        Cooperator co1 = new Cooperator("Дмухановский Антон Антонович", 60, "+7-999-999-10-10", "директор", 180000);
         System.out.printf("ФИО:\t\t%s\nДолжность:\t%s\n\n", co1.getFio(), co1.getPost());
 
         // 5. Создать массив из 5 сотрудников. С помощью цикла вывести информацию только о сотрудниках старше 40 лет;
         Cooperator[] coops = new Cooperator[5];
         coops[0] = co1;
-        coops[1] = new Cooperator("Сквозник-Дмухановская Анна Андреевна", 55, "+7-999-111-11-11", "секретарь", 190000);
+        coops[1] = new Cooperator("Дмухановская Анна Андреевна", 55, "+7-999-111-11-11", "секретарь", 190000);
         coops[2] = new Cooperator("Ляпкин-Тяпкин Аммос Федорович", 45, "+7-998-250-20-00", "главный редактор", 160000);
         coops[3] = new Cooperator("Уховертов Степан Ильич", 35, "+7-998-002-02-02", "обозреватель", 130000);
         coops[4] = new Cooperator("Хлестаков Иван Александрович", 30, "+7-952-350-40-40", "журналист", 100000);
@@ -18,6 +18,7 @@ public class Leeson04HW {
 
         //6. * Создать метод, повышающий зарплату всем сотрудникам старше 45 лет на 5000.
         moreSalary(coops, 45, 5000);
+        moreSalary(coops, 25, 50, 10000);
     }
 
     /**
@@ -39,6 +40,7 @@ public class Leeson04HW {
     /**
      * Повышение зарплаты сотрудниках,
      * чей возраст больше <code>age</code>
+     * посредством метода <code>setSalary()</code> класса <code>Cooperator</code>
      *
      * @param cooperators   массив экземпляров класса <code>Cooperator</code>
      * @param age           возраст сотрудника для повышения ЗП
@@ -49,6 +51,31 @@ public class Leeson04HW {
         for (Cooperator coop : cooperators) {
             if (coop.getAge() > age) {
                 coop.setSalary(coop.getSalary() + salaryDelta);
+                System.out.println(coop.getCoopInfo());
+            }
+        }
+    }
+
+    /**
+     * Повышение зарплаты сотрудниках, чей возраст
+     * больше <code>ageStart</code> и меньше <code>ageFinish</code>
+     * посредством метода <code>moreSalary()</code>
+     * класса <code>Cooperator</code>
+     *
+     * @param cooperators   массив экземпляров класса <code>Cooperator</code>
+     * @param ageStart      возраст сотрудника для повышения ЗП
+     * @param ageFinish     возраст сотрудника для повышения ЗП
+     * @param salaryDelta   сумма повышения ЗП
+     */
+    private static void moreSalary(Cooperator[] cooperators, int ageStart, int ageFinish, int salaryDelta) {
+        if (ageFinish <= ageStart) {
+            System.out.println("Значения возраста для повышения должны быть перданы в порядке повышения и не должны быть равны");
+            return;
+        }
+        System.out.printf("Повышение запрплаты на %d сотрудникам, чей возраст больше %d и меньще %d\n", salaryDelta, ageStart, ageFinish);
+        for (Cooperator coop : cooperators) {
+            if (coop.isAgeBetween(ageStart, ageFinish)) {
+                coop.moreSalary(salaryDelta);
                 System.out.println(coop.getCoopInfo());
             }
         }
