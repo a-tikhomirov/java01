370b3849b10d2f997366908df82d998e49846870
a-tikhomirov <andrey.tikhomirov.88@gmail.com>
2019-11-10 12:44:12 +0300

    lesson 4 task 1

--- lesson04/src/ru/geekbrains/lesson04/hw/Cooperator.java
+++ lesson04/src/ru/geekbrains/lesson04/hw/Cooperator.java
@@ -1,5 +1,10 @@
 package ru.geekbrains.lesson04.hw;
 
 public class Cooperator {
+    private String fio;
+    private int age;
+    private String phoneNumber;
+    private String post;
+    private int salary;
 
 }
