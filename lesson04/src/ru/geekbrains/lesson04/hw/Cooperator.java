da94c7a4c6cd0894b650e062382aac42aa243104
a-tikhomirov <andrey.tikhomirov.88@gmail.com>
2019-11-10 12:47:51 +0300

    lesson 4 task 2

--- lesson04/src/ru/geekbrains/lesson04/hw/Cooperator.java
+++ lesson04/src/ru/geekbrains/lesson04/hw/Cooperator.java
@@ -7,4 +7,12 @@ public class Cooperator {
     private String post;
     private int salary;
 
+    Cooperator(String fio, int age, String phoneNumber, String post, int salary) {
+        this.fio = fio;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+        this.post = post;
+        this.salary = salary;
+    }
+
 }
